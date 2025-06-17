7b2f36b58baf58cc7a397ec611e9a58a9e320a97
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2025-06-17 08:56:19 +0200

    Adding database structure

---
+++ Standalone/Plugins/Crawler/database.sql
@@ -0,0 +1,67 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Channels
+DROP TABLE IF EXISTS `channels`;
+
+CREATE TABLE `channels`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `time_created` datetime NULL DEFAULT NULL,
+  `time_updated` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2322 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_uca1400_ai_ci ROW_FORMAT = Dynamic;
+
+-- Profiles
+DROP TABLE IF EXISTS `profiles`;
+
+CREATE TABLE `profiles`  (
+  `id` int(11) NOT NULL,
+  `city` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `readme` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`readme`)),
+  `birthday` date NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `time_created` datetime NULL DEFAULT NULL,
+  `time_updated` datetime NULL DEFAULT NULL,
+  `hobbies` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`hobbies`)),
+  `music` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`music`)),
+  `movies` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`movies`)),
+  `series` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`series`)),
+  `books` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`books`)),
+  `languages` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL CHECK (json_valid(`languages`)),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_uca1400_ai_ci ROW_FORMAT = Dynamic;
+
+-- Users
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users`  (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NULL DEFAULT NULL,
+  `time_created` datetime NULL DEFAULT NULL,
+  `time_updated` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_time_updated_id`(`time_updated` ASC, `id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_uca1400_ai_ci ROW_FORMAT = Dynamic;
+
+-- Visits
+DROP TABLE IF EXISTS `visits`;
+CREATE TABLE `visits`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NULL DEFAULT NULL,
+  `date` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 57480 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_uca1400_ai_ci ROW_FORMAT = Dynamic;
+
+-- View: Count
+DROP VIEW IF EXISTS `Count`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `Count` AS select format((select count(0) from `users`),0,'de_DE') AS `users`,format((select count(0) from `profiles`),0,'de_DE') AS `profiles`;
+
+-- View: Readmes
+DROP VIEW IF EXISTS `Readmes`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `Readmes` AS select `users`.`id` AS `id`,`users`.`nickname` AS `nickname`,json_unquote(json_extract(`profiles`.`readme`,'$.text.text')) AS `readme` from (`users` join `profiles`) where `users`.`id` = `profiles`.`id` and `profiles`.`readme` is not null;
+
+SET FOREIGN_KEY_CHECKS = 1;
